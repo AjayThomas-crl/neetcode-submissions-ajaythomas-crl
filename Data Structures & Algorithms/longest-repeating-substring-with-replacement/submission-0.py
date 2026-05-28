a13d442624ea0ed5a25cccc97b4e513f0e00c722
K Ajay Thomas <thomaskajay7@gmail.com>
2026-05-28 11:18:04 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l=0
+        r=1
+        a=0
+        while(r<len(s)):
+            if(s[r]==s[l]):
+                a=max(a,r-l+1)
+                r+=1
+            elif(k>0):
+                a=max(a,r-l+1)
+                r+=1
+                k-=1
+            else:
+                l+=1
+        return a
+                
