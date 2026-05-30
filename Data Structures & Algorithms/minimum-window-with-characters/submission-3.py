08a15f2d00a116a90eee20166cc79fcda90f97d9
K Ajay Thomas <thomaskajay7@gmail.com>
2026-05-30 22:42:48 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        m={}
+        for i in t:
+            m[i]=m.get(i,0)+1
+        print(m)
+        l=0
+        r=0
+        c=0
+        m1={}
+        for r in range(len(s)):
+            if(s[r] in m):
+                m[s[r]]-=1
+                print(m)
+                if(m[s[r]]==0):
+                    c+=1
+
+            if(c==len(set(t))):
+                while(s[l] not in m or s[l]==s[l+1]):
+                    l+=1
+                
+                m1[s[l:r+1]]=r-l+1
+                
+                if(s[l] in m):
+                    if(m[s[l]]==0):
+                        c-=1
+                    m[s[l]]+=1
+
+                l+=1
+                    
+                
+        mi=99
+        st=""
+        print(m1)
+        for i,k in m1.items():
+            if(k<mi):
+                mi=k
+                st=i
+        
+        return st
