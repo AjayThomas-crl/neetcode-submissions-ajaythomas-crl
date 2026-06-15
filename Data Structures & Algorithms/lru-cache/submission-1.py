b067277d21aa193a11adfdf250aa3fa9b3683007
K Ajay Thomas <thomaskajay7@gmail.com>
2026-06-16 01:09:43 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key,value):
+        self.val=value
+        self.key=key
+        self.next=self.prev=None
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.m={}
+        self.cap=capacity
+
+        self.left,self.right=Node(0,0),Node(0,0)
+        self.left.next=self.right
+        self.right.prev=self.left
+
+    def remove(self,node):
+        prev,nxt=node.prev,node.next
+        prev.next=nxt
+        nxt.prev=prev
+    def insert(self,node):
+        t=self.right.prev
+        self.right.prev=node
+        node.next=self.right
+        t.next=node
+        node.prev=t
+
+    def get(self, key: int) -> int:
+        if (self.m.get(key)):
+            self.remove(self.m[key])
+            self.insert(self.m[key])
+            return self.m[key].val
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if(key in self.m):
+            self.remove(self.m[key])
+        n=Node(key,value)
+        self.m[key]=n
+        self.insert(n)
+
+        if(len(self.m)>self.cap):
+            del self.m[self.left.next.key]
+            self.remove(self.left.next)
+        
+        return None
+
+
