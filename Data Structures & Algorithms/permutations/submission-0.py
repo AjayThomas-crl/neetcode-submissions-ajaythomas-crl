3905bbcf751a9b05557518ae07b13dbd0ebeea71
Ajay Thomas <thomaskajay7@gmail.com>
2026-06-28 22:32:02 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if (len(nums)==0):
+            return [[]]
+        
+        per=self.permute(nums[1:])
+        res=[]
+        for p in per:
+            for i in range(len(p)+1):
+                p_c=p.copy()
+                p_c.insert(i, nums[0])
+                res.append(p_c)
+        
+        return res
