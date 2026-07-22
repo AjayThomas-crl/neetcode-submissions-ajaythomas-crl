082115b5c589713a9f4de79d4270803b7add57d4
Ajay Thomas <thomaskajay7@gmail.com>
2026-07-22 11:42:01 +0530

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        premap={}
+        
+        for c,p in prerequisites:
+            premap[c]=premap.get(c,[])+[p]
+        
+            
+        
+        vis=set()
+        
+        print(premap)
+        def rec(i):
+            if i>numCourses-1 or i in vis:
+                return False
+            d=premap.get(i,[])
+            if not d:
+                return True
+            vis.add(i)
+            for pre in d:
+                if not rec(pre):
+                    return False
+                else:
+                    print(i,pre)
+                    premap[i].remove(pre)
+            return True
+        ans=True
+        for i in premap:
+            if not rec(i):
+                return False
+        return True
