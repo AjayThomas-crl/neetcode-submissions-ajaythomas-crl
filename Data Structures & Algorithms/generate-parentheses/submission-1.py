ab3aa02b801c455b74a25be22fdb6747038fff38
Ajay Thomas <thomaskajay7@gmail.com>
2026-08-05 00:37:11 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        c=0
+        o=1
+        def rec(s,c,o):
+            if o>n or c>n:
+                return 
+            if c==o and  c==n:
+                res.append(s)
+            
+            if c>o:
+                return
+            
+            rec(s+"(",c,o+1)
+
+            s+=")"
+            c+=1
+            rec(s,c,o)
+        
+        rec("(",0,1)
+        return res
+
+
+
+            
