801678dcc23c33fb52989116b339934a595fde7e
Ajay Thomas <thomaskajay7@gmail.com>
2026-08-31 09:57:55 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        m={5:0,10:0,20:0}
+        for i in bills:
+            m[i]+=1
+            change=i-5
+            if change==0:
+                continue
+            if change==15:
+                if m[5]>=1 and m[10]>=1:
+                    m[5]-=1
+                    m[10]-=1
+                    continue
+                elif m[5]>=3:
+                    m[5]-=3
+                    continue
+                else:
+                    return False
+            elif change==5:
+                if m[5]>=1:
+                    m[5]-=1
+                    continue
+                else:
+                    return False
+
+                
+            
+        return True
