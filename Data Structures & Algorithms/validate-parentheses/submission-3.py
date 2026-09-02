aa6939efbfdd71d755721372ce575eacf4ed5a85
Ajay Thomas <thomaskajay7@gmail.com>
2026-09-02 19:41:44 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        s=deque()
+        m={"(":")","{":"}","[":"]"}
+        for i in s:
+            if i in "{([":
+                s.add(i)
+            else:
+                if i==m[s[-1]]:
+                    s.pop()
+                else:
+                    return False
+        return len(s)==0
+        
+        
